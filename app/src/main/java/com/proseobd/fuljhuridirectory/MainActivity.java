4bba43a3492a41a12b480f6c23341ccb36401b6a
Anirban Prince <anirbanprince@yahoo.com>
2024-10-22 20:17:20 +0600

    Initial commit

--- app/src/main/java/com/proseobd/fuljhuridirectory/MainActivity.java
+++ app/src/main/java/com/proseobd/fuljhuridirectory/MainActivity.java
@@ -1,10 +1,14 @@
 package com.proseobd.fuljhuridirectory;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
 import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.graphics.Typeface;
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar); //Ignore red line errors
--- app/src/main/java/com/proseobd/fuljhuridirectory/UpListFragment.java
+++ app/src/main/java/com/proseobd/fuljhuridirectory/UpListFragment.java
@@ -4,16 +4,32 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 
 public class UpListFragment extends Fragment {
+
+    GridView gridView;
 
     HashMap<String, String> hashMap;
     ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
@@ -23,6 +39,8 @@ public class UpListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View fragmentView = inflater.inflate(R.layout.fragment_up_list, container, false);
+
+        gridView = fragmentView.findViewById(R.id.gridView);
 
 
 
@@ -45,7 +63,7 @@ public class UpListFragment extends Fragment {
 
         @Override
         public int getCount() {
-            return 5;
+            return arrayList.size();
         }
 
         @Override
@@ -73,5 +91,83 @@ public class UpListFragment extends Fragment {
     //===================== Base Adapter End Here ==========================//
     //===================== Base Adapter End Here ==========================//
 
+    /////////////////////////////////////////////////////////////////////////
+
+    //===================== Data Parsing Privet Methode ====================//
+    //===================== Data Parsing Privet Methode ====================//
+    //===================== Data Parsing Privet Methode ====================//
+
+    private void createTable(){
+
+        arrayList = new ArrayList<>();
+
+
+        String url = "https://proseobd.com/apps/fuljhuridirectory/upmembers/up_members.json";
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.POST, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+
+
+                        Log.d("ServerRes", response.toString());
+
+                        for (int x=0; x<response.length(); x++){
+
+                            try {
+
+                                JSONObject jsonObject = response.getJSONObject(x);
+
+                                String name =  jsonObject.optString("name");
+                                String designation = jsonObject.optString("designation");
+                                String phone_number = jsonObject.optString("phone_number");
+                                String word_no = jsonObject.optString("word_no");
+                                String image_link = jsonObject.optString("image_link");
+                                String email = jsonObject.optString("email");
+
+                                hashMap = new HashMap<>();
+                                hashMap.put("name", name);
+                                hashMap.put("designation", designation);
+                                hashMap.put("phone_number", phone_number);
+                                hashMap.put("word_no", word_no);
+                                hashMap.put("image_link", image_link);
+                                hashMap.put("email", email);
+                                arrayList.add(hashMap);
+
+
+
+                                MyAdapter myAdapter = new MyAdapter();
+                                gridView.setAdapter(myAdapter);
+
+
+                            } catch (JSONException e) {
+                                throw new RuntimeException(e);
+                            }
+
+
+
+                        }
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(jsonArrayRequest);
+
+
+
+
+    }
+
+    //===================== Data Parsing Privet Methode ====================//
+    //===================== Data Parsing Privet Methode ====================//
+    //===================== Data Parsing Privet Methode ====================//
 
 }

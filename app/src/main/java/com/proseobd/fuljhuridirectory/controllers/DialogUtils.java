ea29e285e4b8fdc8e87d0aec24c0b36e6a72f18b
Anirban Prince <anirbanprince@yahoo.com>
2024-11-02 11:21:52 +0600

    Initial commit

---
+++ app/src/main/java/com/proseobd/fuljhuridirectory/controllers/DialogUtils.java
@@ -0,0 +1,19 @@
+package com.proseobd.fuljhuridirectory.controllers;
+
+import android.app.AlertDialog;
+import android.content.Context;
+
+public class DialogUtils {
+    // Public static method to show an AlertDialog
+    public static void showAlertDialog(Context context, String title, String message) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setPositiveButton("OK", null)
+                .setNegativeButton("Cancel", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+}
+

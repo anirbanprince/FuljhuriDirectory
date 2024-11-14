33a7f5d8d90ee30c2324306b5875a3a4b4fe0f7b
Anirban Prince <anirbanprince@yahoo.com>
2024-11-14 23:21:24 +0600

    Notification

--- app/src/main/java/com/proseobd/fuljhuridirectory/HomeFragment.java
+++ app/src/main/java/com/proseobd/fuljhuridirectory/HomeFragment.java
@@ -1,7 +1,13 @@
 package com.proseobd.fuljhuridirectory;
 
+import android.app.AlertDialog;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,6 +42,8 @@ public class HomeFragment extends Fragment {
 
         MyAdapter myAdapter = new MyAdapter();
         gridView.setAdapter(myAdapter);
+
+        askNotificationPermission();
 
 
 
@@ -444,10 +452,50 @@ public class HomeFragment extends Fragment {
         hashMap.put("img" , String.valueOf(R.drawable.more));
         arrayList.add(hashMap);
 
-
-
-
     }
+
+
+    ///////////// Notification Start ////////////
+
+    // Declare the launcher at the top of your Activity/Fragment:
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // FCM SDK (and your app) can post notifications.
+                } else {
+                    // TODO: Inform user that that your app will not show notifications.
+                }
+            });
+
+
+    private void askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+
+            if (ContextCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+
+            }
+            else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+
+                new AlertDialog.Builder(requireActivity())
+                        .setTitle("Notification Permission")
+                        .setMessage("This app will not show notifications without permission")
+                        .setPositiveButton("OK", (dialog, which) -> requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS))
+                        .setNegativeButton("Cancel", null)
+                        .show();
+
+            }
+            else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
+    }
+
+    ///////////// Notification END ////////////
+
 
 
 
